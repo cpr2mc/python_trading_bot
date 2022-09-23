9ce9a3dda39d51e4b9c17317d51f944b768079b7
Your Name <roos.chris.p@gmail.com>
2022-09-23 14:18:28 -0500

    changed secrets to api_secrets.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas
 import xlsxwriter
 import requests
 import math
-from secrets import IEX_API_CLOUD_TOKEN
+from api_secrets import IEX_CLOUD_API_TOKEN
 
 stocks = pandas.read_csv('sp_500_stocks.csv')
 
